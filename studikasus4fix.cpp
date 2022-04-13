d8c908905d97788678518058a549e7cd62ff1d51
nandalestari <nndalstari27@gmail.com>
2022-04-13 19:20:42 +0700

    studi kasus 4

---
+++ studikasus4fix.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+
+using namespace std;
+
+class Tabungan{
+	public:
+		void cetak();
+		int akumulasi(int a, int pendapatan);
+};
+
+
+int Tabungan::akumulasi(int a, int pendapatan){
+	int n;
+	int x;
+	int hasil;
+	
+
+}
