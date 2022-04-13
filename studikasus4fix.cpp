247b20178ced7fcb283927dca7477faa863abca5
BintiSholikhah <bintisholikhah95@gmail.com>
2022-04-13 21:50:40 +0700

    manambah kode

--- studikasus4fix.cpp
+++ studikasus4fix.cpp
@@ -10,11 +10,18 @@ class Tabungan{
 		int akumulasi(int a, int pendapatan);
 };
 
-
 int Tabungan::akumulasi(int a, int pendapatan){
 	int n;
 	int x;
 	int hasil;
-	
-
+	if(a > 0, a <= 10){ 
+		cout <<"\t\t";
+		cin >> n;
+		cout << "\t\t\t\t";
+		cin >>x;
+		hasil = pendapatan-x+n;
+		cout << "| " << a << "\t   |" << "\t\t" << "  |\t" << "\t  |\t" << hasil << "\t\t|" << endl;
+		cout << "___________________________________________________________________\n";
+		return akumulasi(a+1, pendapatan-x+n);
+	}
 }
