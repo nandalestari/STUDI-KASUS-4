1bd0feb56f0e331807877a26b54478491d3bf90b
Aida Azzahra Rachmawati <aidaazzahra27@gmail.com>
2022-04-13 22:03:57 +0700

    kode lanjutan

--- studikasus4fix.cpp
+++ studikasus4fix.cpp
@@ -25,3 +25,19 @@ int Tabungan::akumulasi(int a, int pendapatan){
 		return akumulasi(a+1, pendapatan-x+n);
 	}
 }
+void Tabungan::cetak(){
+	int jumlahdapat;
+	cout << "==================== AKUMULASI TABUNGAN ========================\n ";
+	cout << "              dalam kurun waktu 10 hari\n";
+	cout << "================================================================\n";
+	cout << "\n";
+	cout << "| Hari ke |   Tabungan   |   Pengeluaran  |   Total Tabungan  |\n";
+	cout << "---------------------------------------------------------------\n";
+	akumulasi(1, jumlahdapat);
+}
+int main (){
+	Tabungan Sarah;
+	Sarah.cetak();
+	Sarah.akumulasi(11,0);
+	return 0;
+}
